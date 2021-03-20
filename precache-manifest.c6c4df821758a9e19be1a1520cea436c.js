eb0080801b54906a2266e7d9965431640fdcb385
Harut Muqoyan <hmuqoyan@gmail.com>
2021-03-20 20:56:59 +0400

    Updates

---
+++ precache-manifest.c6c4df821758a9e19be1a1520cea436c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3764c9816598b085ef4f49ca80acfd9c",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "31b77415080da8839891",
+    "url": "/pokemon/static/css/main.5014c947.chunk.css"
+  },
+  {
+    "revision": "058f608fe83a8a5884ab",
+    "url": "/pokemon/static/js/2.2c9d194c.chunk.js"
+  },
+  {
+    "revision": "31b77415080da8839891",
+    "url": "/pokemon/static/js/main.6652783f.chunk.js"
+  },
+  {
+    "revision": "7d7283451d0e75fc859b",
+    "url": "/pokemon/static/js/runtime-main.68435288.js"
+  }
+]);
